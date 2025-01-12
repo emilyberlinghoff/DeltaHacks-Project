3beebd523aa5901fee870d11b3f2d7764f4ae677
Alan Sanjit <121334522+AlanSanjit@users.noreply.github.com>
2025-01-12 10:39:08 -0500

    Update DeltaHacks_ProjectApp.swift

--- DeltaHacks-Project/DeltaHacks Project/DeltaHacks_ProjectApp.swift
+++ DeltaHacks-Project/DeltaHacks Project/DeltaHacks_ProjectApp.swift
@@ -1,9 +1,13 @@
 import SwiftUI
 
+// Main entry point of the SwiftUI application
 @main
 struct DeltaHacks_ProjectApp: App {
+    // The body property defines the app's user interface and behavior
     var body: some Scene {
+        // WindowGroup is the default container for the app's content in SwiftUI
         WindowGroup {
+            // The root view of the app, where the app's UI starts
             ContentView()
         }
     }
